63a9f9fd99c5496eb84845f4784dabed4d968ff0
Mandy Morris <mjmorris@udel.edu>
2015-11-13 15:02:28 -0500

    Initial Commit

---
+++ PS6_DAL/src/test/java/base/Person_Test.java
@@ -0,0 +1,106 @@
+package base;
+
+import static org.junit.Assert.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import domain.PersonDomainModel;
+
+
+public class Person_Test {
+
+	private static PersonDomainModel per1;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		
+		Date dDate = null;
+		try {
+			dDate = new SimpleDateFormat("yyyy-MM-dd").parse("1972-07-32");
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		per1 = new PersonDomainModel();
+		per1.setFirstName("Tom");
+		per1.setFirstName("Smith");
+		per1.setBirthday(dDate);
+		per1.setCity("Newark");
+		per1.setPostalCode(19717);
+		per1.setStreet("123 Rose Lane");
+		
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		PersonDomainModel per;
+		PersonDAL.deletePerson(per1.getPersonID());
+		per = PersonDAL.getPerson(per1.getPersonID());
+		assertNull("The Person shouldn't have been in the database",per);
+	}
+
+	@Test
+	public void AddPersonTest() {
+		
+		PersonDomainModel per;
+		per = PersonDAL.getPerson(per1.getPersonID());
+		assertNull("The Person shouldn't have been in the database",per);
+		PersonDAL.addPerson(per1);
+		
+		per = PersonDAL.getPerson(per1.getPersonID());
+		System.out.println(per1.getPersonID() + " found");
+		assertNotNull("The Person should have been added to the database",per);
+	}
+	
+	@Test
+	public void updatePersonTest() {
+		
+		PersonDomainModel per;
+		final String C_LASTNAME = "Jones";
+		
+		per = PersonDAL.getPerson(per1.getPersonID());
+		assertNull("The Person shouldn't have been in the database",per);
+		PersonDAL.addPerson(per1);
+		
+		per1.setLastName(C_LASTNAME);
+		PersonDAL.updatePerson(per1);
+		
+		per = PersonDAL.getPerson(per1.getPersonID());
+		
+		assertTrue("Name Didn't Change",per1.getLastName() == C_LASTNAME);
+	}
+	
+	@Test
+	public void deletePersonTest() {
+		
+		PersonDomainModel per;
+		per = PersonDAL.getPerson(per1.getPersonID());
+		assertNull("The Person shouldn't have been in the database",per);
+		
+		PersonDAL.addPerson(per1);
+		per = PersonDAL.getPerson(per1.getPersonID());
+		System.out.println(per1.getPersonID() + " found");
+		assertNotNull("The Person should have been added to the database",per);
+		
+		PersonDAL.deletePerson(per1.getPersonID());
+		per = PersonDAL.getPerson(per1.getPersonID());
+		assertNull("The Person shouldn't have been in the database",per);
+	}
+
+}
